8556575bf9227e8cf16455f275491add5e1d0bde
Pablo Qu <pablo.quagliata@gmail.com>
2021-03-20 12:57:39 -0300

    WIP

--- DelegationPattern/DelegationPattern/UI/FirstViewController.swift
+++ DelegationPattern/DelegationPattern/UI/FirstViewController.swift
@@ -10,28 +10,54 @@ import UIKit
 class FirstViewController: UIViewController {
 
     // MARK: - Outlets
-    private lazy var containerView: UIView = {
+
+    private lazy var topContainerView: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .gray
+        view.backgroundColor = .systemTeal
         return view
+    }()
+
+    private lazy var bottomContainerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.setContentHuggingPriority(.defaultHigh + 1, for: .vertical)
+        //view.setContentCompressionResistancePriority(.defaultHigh + 1, for: .vertical)
+        view.backgroundColor = .red
+        return view
+    }()
+
+    private lazy var stackView: UIStackView = {
+        let verticalStack = UIStackView()
+        verticalStack.translatesAutoresizingMaskIntoConstraints = false
+        verticalStack.axis = .vertical
+        verticalStack.distribution  = UIStackView.Distribution.fill
+        return verticalStack
     }()
 
     private lazy var textLabel: UILabel = {
         var label = UILabel()
+        label.text = "HOLA"
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.textColor = .white
+        label.font = UIFont.boldSystemFont(ofSize: 18)
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
 
     private lazy var routerButton: UIButton = {
         let button = UIButton()
+        button.setTitle("HOLABUTTON", for: .normal)
+
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
 
-    var safeArea: UILayoutGuide
-
-    let containerPadding: CGFloat = 10
+    // MARK: - Properties
+    let containerYPadding: CGFloat = 30
+    let stackXPadding: CGFloat = 50
+    let stackYPadding: CGFloat = 300
 
     // MARK: - Life cycle
     override func viewDidLoad() {
@@ -45,19 +71,40 @@ class FirstViewController: UIViewController {
 extension FirstViewController {
 
     private func setupViews() {
-
+        // containerView within parent view
+        view.addSubview(stackView)
+        stackView.addArrangedSubview(topContainerView)
+        topContainerView.addSubview(textLabel)
+        stackView.addArrangedSubview(bottomContainerView)
+        bottomContainerView.addSubview(routerButton)
     }
 
     private func setupConstraints() {
-        safeArea = view.safeAreaLayoutGuide
+        // we use safeArea layouts for positioning the containerView
+        let safeArea = view.safeAreaLayoutGuide
+
+        // setting stack constraints
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: safeArea.topAnchor,
+                                           constant: stackYPadding),
+            stackView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor,
+                                               constant: stackXPadding),
+            stackView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor,
+                                                constant: -stackXPadding),
+            stackView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor,
+                                              constant: -stackYPadding)
+        ])
 
         NSLayoutConstraint.activate([
-            containerView.topAnchor.constraint(equalTo: safeArea.topAnchor,
-                                               constant: containerPadding),
-            containerView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor,
-                                                   constant: containerPadding),
-            containerView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -containerPadding),
-            containerView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: -containerPadding)
+            bottomContainerView.heightAnchor.constraint(equalToConstant: 80)
+        ])
+
+        NSLayoutConstraint.activate([
+            textLabel.centerXAnchor.constraint(equalTo: topContainerView.centerXAnchor),
+            textLabel.centerYAnchor.constraint(equalTo: topContainerView.centerYAnchor,
+                                               constant: -containerYPadding),
+            routerButton.centerXAnchor.constraint(equalTo: bottomContainerView.centerXAnchor),
+            routerButton.centerYAnchor.constraint(equalTo: bottomContainerView.centerYAnchor)
         ])
     }
 }
